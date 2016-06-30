555bffd41eee73efc6ab36b90bae9b8e5d0452cc
Steven U <steven.h.u@outlook.com>
2016-06-29 19:15:28 -0700

    Defining Server load post endpoint

---
+++ ServerTrack.WebApi/Controllers/ServerController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using ServerTrack.WebApi.Models;
+
+namespace ServerTrack.WebApi.Controllers
+{
+    [RoutePrefix("api/server")]
+    public class ServerController : ApiController
+    {
+        [HttpPost]
+        [Route("{serverName}")]
+        public void Post(string serverName, [FromBody]ServerLoadEntry serverLoadEntry)
+        {
+            
+        }
+    }
+}
---
+++ ServerTrack.WebApi/Models/ServerLoadEntry.cs
@@ -0,0 +1,8 @@
+﻿namespace ServerTrack.WebApi.Models
+{
+    public class ServerLoadEntry
+    {
+        public double CpuLoad { get; set; }
+        public double RamLoad { get; set; }
+    }
+}
