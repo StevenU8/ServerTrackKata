b08d20a1e4b0beff86ebdce5f06b6705f25a0f87
Steven U <steven.h.u@outlook.com>
2016-06-29 19:32:10 -0700

    Adding test for httpresponse code

---
+++ ServerTrack.WebApi.Tests/ServerControllerPostTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using ServerTrack.WebApi.Controllers;
+using ServerTrack.WebApi.Models;
+
+namespace ServerTrack.WebApi.Tests
+{
+    [TestFixture]
+    public class ServerControllerPostTests
+    {
+        [Test]
+        public void ServerController_Post_Returns200OnSuccess()
+        {
+            var serverLoadData = new ServerLoadEntry
+            {
+                CpuLoad = 1,
+                RamLoad = 2
+            };
+
+            var controller = new ServerController();
+
+            var response = controller.Post("testServer", serverLoadData);
+
+            Assert.That(response.StatusCode, Is.EqualTo(200));
+        }
+    }
+}
